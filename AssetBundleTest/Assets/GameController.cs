930e9df427b14111de62a5e60c2f5f6ce36848ea
OhHiMark <Zervanseryi@gmail.com>
2020-03-23 11:17:07 +0200

    2.0

--- AssetBundleTest/Assets/GameController.cs
+++ AssetBundleTest/Assets/GameController.cs
@@ -12,31 +12,42 @@ public class GameController : Singleton<GameController>
     [SerializeField]
     private DataManager DataManager;
 
+    #if UNITY_EDITOR
     private void Awake()
     {
         //Only after loading a new bundles
         DataManager.SaveAssetBundles();
     }
+    #endif  
+
     public void InitFigure()
     {
-        DataManager.Load();
-        List<string> _assetNames = DataManager.PlayerData.AssetNames;
-        int random = Random.Range(0, _assetNames.Count);
-        assetBundleManager.LoadAssetBundle(_assetNames[random].ToLower());
-
-        GeometryObjectData geometryData = Resources.Load<GeometryObjectData>("Installers/GeometryObjectData");
-        GeometryObjectModel asset = assetBundleManager.FindMyAssetFromBundle(geometryData);
-        if (asset != null)
+        try
         {
-            Instantiate(asset);
-            GameData gameData = Resources.Load<GameData>("Installers/GameData");
-            timer = new Timer(gameData.ObservableTime, asset.ChangeColor);
-            print("Time to change color: " + gameData.ObservableTime);
-            print("Min click count:" + asset.ClickData.MinClicksCount);
-            print("Max click count:" + asset.ClickData.MaxClicksCount);
-            timer.Restart();
+            DataManager.Load();
+            List<string> _assetNames = DataManager.PlayerData.AssetNames;
+            int random = Random.Range(0, _assetNames.Count);
+            assetBundleManager.LoadAssetBundle(_assetNames[random].ToLower());
+
+            GeometryObjectData geometryData = Resources.Load<GeometryObjectData>("Installers/GeometryObjectData");
+            GeometryObjectModel asset = assetBundleManager.FindMyAssetFromBundle(geometryData);
+            if (asset != null)
+            {
+                Instantiate(asset);
+                GameData gameData = Resources.Load<GameData>("Installers/GameData");
+                timer = new Timer(gameData.ObservableTime, asset.ChangeColor);
+                print("Time to change color: " + gameData.ObservableTime);
+                print("Min click count:" + asset.ClickData.MinClicksCount);
+                print("Max click count:" + asset.ClickData.MaxClicksCount);
+                timer.Restart();
+            }
+            else
+                print("Asset doesn't exist in loaded bundle");
         }
-        else
-            print("Asset doesn't exist in loaded bundle");
+
+        catch (System.NullReferenceException e)
+        {
+            print ("First Save some bundles through Assets/BuildAssets");
+        }
     }
 }
